c885daa153072a574f672d0306962a688e31aaf2
Kunal Ganglani <kunalganglani@gmail.com>
2018-03-10 11:02:47 +0530

    user service creation

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'
+import { map } from 'rxjs/operator/map';
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+  private _userUrl = '../assets/user.json';
 
+  constructor(private _http: Http) { }
+
+  getUser(id: number){
+    return this._http.get(this._userUrl).map((response) => 
+    response.json());
+  }
 }
