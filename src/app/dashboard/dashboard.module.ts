2652c50ec546986e85ef4af82e3bd257ec6c7523
Kunal Ganglani <kunalganglani@gmail.com>
2018-03-31 18:44:28 +0530

    material module added, material style added

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule} from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { AuthGuard } from '../auth/services/auth.gaurd';
+import { MaterialModule } from '../shared/material.module';
 
 const routes: Routes = [{
   path: '',
@@ -16,6 +17,7 @@ const routes: Routes = [{
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
     RouterModule.forChild(routes)
   ],
   declarations: [
