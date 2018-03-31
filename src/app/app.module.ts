987e116571968ecf756d9a02f973fbce72de0a8e
Kunal Ganglani <kunalganglani@gmail.com>
2018-03-31 14:48:36 +0530

    login and dashbard modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { UserService } from './user.service';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard'},
+  {path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule'},
+  {path: 'login', loadChildren: './login/login.module#LoginModule'},
+  {path: '**', redirectTo: 'login'}
+];
 
 
 @NgModule({
@@ -14,9 +22,10 @@ import { FooterBarComponent } from './footer-bar/footer-bar.component';
     FooterBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [ UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
