02f4b44f59c04761414bc59e345904a0323f1808
Kunal Ganglani <kunalganglani@gmail.com>
2018-04-01 18:33:14 +0530

    login component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
@@ -24,6 +25,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule
     AuthModule,
     RouterModule.forRoot(routes),
   ],
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MaterialModule } from '../shared/material.module';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: []
+  declarations: [LoginComponent]
 })
 export class LoginModule { }
